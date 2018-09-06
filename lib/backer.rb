fe3fb68fd17a25a4ec120c9377beff936a128a03
Cedric David <cedric.david@btopenworld.com>
2018-09-06 03:34:54 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer
-  attr_accessor :name, :backed_projects
+
+  attr_reader :name, :backed_projects
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
-  
-  def back_project(project)    
-    @backed_projects << project 
+  def back_project(project)
+    @backed_projects << project
     project.backers << self
   end
 
--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,12 @@
 class Project
-  attr_accessor :title, :backers
+  attr_reader :title, :backers
 
   def initialize(title)
     @title = title
     @backers = []
   end
-
   def add_backer(backer)
-    @backers << backer  
+    @backers << backer
     backer.backed_projects << self
   end
-  
 end
