99bb8223b8808f0bc9c54822ef0dd6ffdae9c34f
Cedric David <cedric.david@btopenworld.com>
2018-09-06 03:19:26 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,3 @@
 class Project
-  attr_accessor :title, :backers
 
-  def initialize(title)
-    @title = title
-    @backers = []
-  end
-
-  def add_backer(backer)
-    @backers << backer  
-    backer.backed_projects << self
-  end
-  
 end
